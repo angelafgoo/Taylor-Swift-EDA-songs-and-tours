e92db080751c1493e8fbbe1c9813da2564fb4b47
angelafgoo <154274570+angelafgoo@users.noreply.github.com>
2024-01-17 11:33:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-#import plotly.graph_objects as go
+import plotly.graph_objects as go
 import plotly.express as px
 from streamlit_option_menu import option_menu
 import folium
